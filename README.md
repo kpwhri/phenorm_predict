cb5df6d19301a1dad8a6a4667148104c252aae09
dcronkite <dcronkite@gmail.com>
2024-09-16 15:02:33 -0700

    Copied from Sentinel Scalable NLP

---
+++ README.md
@@ -0,0 +1,21 @@
+# PheNorm
+
+An R implementation of PheNorm.
+
+## Overview
+
+The following is an R implementation of PheNorm. This repository has been set up to facilitate applying the PheNorm algorithm (rather than building). 
+
+## Applying PheNorm Algorithm
+
+### Install Required Packages: `install_packages.R`
+
+Run the `install_packages.R` script to ensure that your R environment has the necessary prerequisites installed to run PheNorm.
+
+    Rscript install_packages.R
+
+### Prepare Dataset: `process_data.R`
+
+Run the `process_data.R` script to prepare and format the dataset.
+
+
---
+++ install_packages.R
@@ -0,0 +1,11 @@
+# install necessary packages for the COVID PheNorm analysis
+
+package_list <- c("optparse", "here", "PheNorm", "ROCR", "WeightedROC", "cvAUC", "vimp")
+tidy_package_list <- c("ggplot2", "dplyr", "tidyr", "readr", "stringr", "purrr", "cowplot", "haven")
+# check to make sure each package is installed; if not, install it
+lapply(as.list(c(package_list, tidy_package_list)), function(package) {
+  is_package_avail <- require(package, character.only = TRUE)
+  if (!is_package_avail) {
+    install.packages(package, repos = "https://cran.rstudio.com", INSTALL_opts = "--no-lock")
+  }
+})
